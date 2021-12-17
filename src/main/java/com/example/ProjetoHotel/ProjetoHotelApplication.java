19323986441bc6f81908d39d22683954e61b30b7
AndreAraujo1998 <aflorencio@itelinfo.com>
2021-12-17 08:10:26 -0400

    primeiro commit

---
+++ src/main/java/com/example/ProjetoHotel/ProjetoHotelApplication.java
@@ -0,0 +1,17 @@
+package com.example.ProjetoHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoHotelApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Inicializando o projeto");
+		SpringApplication.run(ProjetoHotelApplication.class, args);
+		System.out.println("Projeto inicializado");
+
+
+	}
+
+}
