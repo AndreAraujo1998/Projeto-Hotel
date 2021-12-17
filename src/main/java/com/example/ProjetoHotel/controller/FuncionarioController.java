43d5e59ba5f2bee5839438549f924fc092bcd5b4
AndreAraujo1998 <aflorencio@itelinfo.com>
2021-12-17 10:27:58 -0400

    Adicionando Controller de Funcionario

---
+++ src/main/java/com/example/ProjetoHotel/controller/FuncionarioController.java
@@ -0,0 +1,65 @@
+package com.example.ProjetoHotel.controller;
+
+
+import com.example.ProjetoHotel.entities.Funcionario;
+import com.example.ProjetoHotel.repositories.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("Funcionario")
+public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @GetMapping()
+    public List<Funcionario> listar(){
+        List<Funcionario> lista = funcionarioRepository.findByAtivo(true);
+        return lista;
+    }
+
+    //Retorna 1
+    @GetMapping("/{id}") //Chaves significam parametro da URL
+    public Funcionario buscar(@PathVariable int id) {    //É preciso colocar o path variable para conseguir pegar da url o parametro inserido
+        Funcionario aposta = funcionarioRepository.findById(id).get();
+        return aposta;
+    }
+
+    @PostMapping //Mapeia o POST na URL
+    public Funcionario incluir(@RequestBody Funcionario funcionario) {
+        funcionario.setIdFuncionario(0);
+        funcionarioRepository.save(funcionario);
+        funcionarioRepository.flush(); //Comando para gravar a nova aposta efetivamente
+        return funcionario;
+    }
+
+    @PutMapping  //Mapeia o POST na URL
+    public Funcionario alterar(@RequestBody Funcionario funcionario) {
+        funcionarioRepository.save(funcionario);
+        funcionarioRepository.flush(); //Comando para gravar a nova aposta efetivamente
+        return funcionario;
+    }
+
+    @DeleteMapping
+    public Funcionario deletar(@RequestBody Funcionario funcionario) {
+        Funcionario excluir = funcionarioRepository.findById(funcionario.getIdFuncionario()).get();
+        excluir.setAtivo(false);
+        funcionarioRepository.save(excluir);
+        funcionarioRepository.flush();
+        return excluir;
+    }
+
+    @DeleteMapping("{id}")
+    public Funcionario deletar(@PathVariable Integer id) { //Para funcionar, é preciso colocar id na URL e o JSON pode ir vazio
+        Funcionario excluir = funcionarioRepository.findById(id).get();
+        excluir.setAtivo(false);
+        funcionarioRepository.save(excluir);
+        funcionarioRepository.flush();
+        return excluir;
+    }
+
+
+}
--- src/main/java/com/example/ProjetoHotel/repositories/PratoRepository.java
+++ src/main/java/com/example/ProjetoHotel/repositories/PratoRepository.java
@@ -2,9 +2,11 @@ package com.example.ProjetoHotel.repositories;
 
 import com.example.ProjetoHotel.entities.Prato;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PratoRepository extends JpaRepository<Prato, Integer> {
 
     List<Prato> findByAtivo(Boolean ativo);
