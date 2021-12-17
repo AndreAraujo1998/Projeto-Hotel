dffcb923bc0448a72c644eb0cf91da324669d969
henriquelopes42 <lsiqueira@itelinfo.com>
2021-12-17 10:24:21 -0400

    adicionado repository Quarto

---
+++ src/main/java/com/example/ProjetoHotel/repositories/QuartoRepository.java
@@ -0,0 +1,14 @@
+package com.example.ProjetoHotel.repositories;
+
+import com.example.ProjetoHotel.entities.Quarto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuartoRepository extends JpaRepository<Quarto,Integer> {
+
+    List<Quarto> findByAtivo(Boolean ativo);
+
+}
