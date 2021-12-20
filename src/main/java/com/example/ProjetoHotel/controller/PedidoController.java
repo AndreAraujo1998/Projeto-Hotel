79bf47c8e790b2d5a62aaf2480cdd038d30202bb
morgenstern28 <jmedeiros@itelinfo.com>
2021-12-20 10:34:31 -0400

    a procura de uma att.

--- src/main/java/com/example/ProjetoHotel/controller/PedidoController.java
+++ src/main/java/com/example/ProjetoHotel/controller/PedidoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("pedido")
+@RequestMapping("Pedido")
 public class PedidoController {
 
     @Autowired
