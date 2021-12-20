b1591e41dfc6462cc64ccce7b7e9ac6cf58f13fb
Crivelente <mcrivelente@itelinfo.com>
2021-12-20 11:09:38 -0400

    Adicionando validação

--- src/main/java/com/example/ProjetoHotel/entities/Funcionario.java
+++ src/main/java/com/example/ProjetoHotel/entities/Funcionario.java
@@ -11,7 +11,6 @@ public class Funcionario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idFuncionario", nullable = false, length = 10 )
     @Pattern(regexp ="[0-9]{10}", message = "O codigo de ser um numero inteiro")
-
     private Integer IdFuncionario;
 
 
@@ -19,13 +18,16 @@ public class Funcionario {
     @Pattern(regexp = "[A-z]{10,50}", message = "O nome deve ter entre 10 e 50 caracteres")
     private String nome;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, length = 50)
+    @Pattern(regexp = "^([0-9A-Za-z]+)(@{1})([A-Za-z]+)(.)([A-Za-z]{2,3})$", message = "O e-mail deve ser válido")
     private String email;
 
-    @Column(name="telefone")
+    @Column(name="telefone", nullable = false, length = 15)
+    @Pattern(regexp = "^\\(\\d{2}\\)\\s[0-9]{5}-[0-9]{4}$\n", message = "O número de celular deve ser válido")
     private String telefone;
 
-    @Column(name="idSalario")
+    @Column(name="idSalario", nullable = false, length = 10 )
+    @Pattern(regexp = "^[0-9]{10}$", message = "O código deve ser um número inteiro")
     private Float idSalario;
 
     @Column(name= "ativo")
